4ea80965019ec3f8ad1de893756358f1bb220da6
Pratyush <connect2pratyush@gmail.com>
2023-04-14 03:11:48 +0530

    initial

---
+++ main.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI
+import requests
+from bs4 import BeautifulSoup
+
+app = FastAPI()
+
+def makeDirectory():
+    url = "https://blockworks.co/grants"
+    resp = requests.get(url,     headers = {
+            'User-Agent': 'Popular browser\'s user-agent',
+        })
+    html = resp.content
+
+    soup = BeautifulSoup(html, 'html.parser')
+
+    table = soup.find_all('table')[0]
+    tbody = table.find_all('tbody')[0]
+    grants = tbody.find_all('tr')
+
+    directory = []
+    keys = ["project", "details", "type", "category", "deadline", "apply"]
+
+    for grant in grants:
+        vals = grant.find_all('td')
+        rec = {}
+        for i,j in enumerate(vals):
+            if i!=5:
+                rec[keys[i]]=j.text
+            # else:
+                # a = j.find_all("a")[0]
+                # print(i,rec)
+                # rec[keys[i]]=a['href']
+        directory.append(rec)
+    
+    return directory
+
+def searchCateg(direc, categ):
+    trueList = []
+    for i in direc:
+        if categ in i["category"]:
+            trueList.append(i)
+    return trueList
+
+@app.post("/findGrants")
+async def findGrants(body: dict):
+    # Do something with the payload
+    direc = makeDirectory()
+    categ = body['category']
+    lis = searchCateg(direc, categ)
+    print(lis)
+
+    return {"grants": lis}
